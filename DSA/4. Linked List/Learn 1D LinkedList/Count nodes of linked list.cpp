c82f96a008b6565f94a3a4e1d32b49d7a99b73e8
preetsagar <ps792001@gmail.com>
2023-05-27 20:15:02 +0530

    6.1

--- DSA/4. Linked List/Learn 1D LinkedList/Count nodes of linked list.cpp
+++ DSA/4. Linked List/Learn 1D LinkedList/Count nodes of linked list.cpp
@@ -1,19 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 struct Node
 {
     int data;
-    Node* next;
-    Node(int x) {  data = x;  next = NULL; }
-}; */
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
 
 class Solution
 {
-    public:
-    //Function to count nodes of a linked list.
-    int getCount(struct Node* head){
-    
-        //Code here
-        int count=0;
-        while(head){
+public:
+    // Function to count nodes of a linked list.
+    int getCount(struct Node *head)
+    {
+
+        // Code here
+        int count = 0;
+        while (head)
+        {
             count++;
             head = head->next;
         }
